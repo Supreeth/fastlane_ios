b02a87e4342d3e885c692f8e267ec069e41cc21e
Supreeth <supreethd@maveric-systems.com>
2020-02-18 21:12:09 +0530

    init

---
+++ FastLaneExmaple/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FastLaneExmaple
+//
+//  Created by Supreethd on 18/02/20.
+//  Copyright © 2020 Supreethd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
