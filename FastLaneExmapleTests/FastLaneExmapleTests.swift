5e26f7954aa1d70bf8964a9f927219489dc2a5f9
Supreeth <supreethd@maveric-systems.com>
2020-02-19 13:14:39 +0530

    added sample tests

--- FastLaneExmapleTests/FastLaneExmapleTests.swift
+++ FastLaneExmapleTests/FastLaneExmapleTests.swift
@@ -22,13 +22,17 @@ class FastLaneExmapleTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let testBool = true
+        XCTAssertTrue(testBool)
     }
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+        let testBool = true
+        XCTAssertTrue(testBool)
     }
 
 }
